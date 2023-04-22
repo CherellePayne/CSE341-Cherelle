678972172c346cb74707a241965caf51071585fa
CherellePayne <cherellelpayne@gmail.com>
2023-04-22 13:39:38 -0600

    2nd commit

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+const routes = require('express').Router();
+
+routes.get('/', (req, res) => {
+  res.send('KenPayne');
+});
+
+//From video
+// const { awesomeFunction, returnAnotherPerson } = require('../controllers');
+
+// const routes = require('express').Router();
+
+// const myController = require('/Users/kenpayne/Documents/CSE341/CSE341-Cherelle/routes/index.js')
+// routes.get('/', myController, awesomeFunction);
+// routes.get('/awesome',myController, returnAnotherPerson);
+
+module.exports = routes;
+
+
+
