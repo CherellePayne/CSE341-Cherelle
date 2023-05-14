f0e4350a6142b4c325e53331b926f6d3897fee23
CherellePayne <cherellelpayne@gmail.com>
2023-05-13 19:59:36 -0600

    This Commit has Swagger aded to the package file

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-//stretch assignment
-// const MongoClient = require('mongodb').MongoClient;
 const mongodb = require('./db/connect');
-//stretch assignment
-// const professionalRoutes = require('./routes/professional');
 
 //listening to decide what "door" render wants to use, or use port 8080, or any port that isn't already in use.
 const port = process.env.PORT || 8080;
@@ -13,12 +9,18 @@ const app = express();
 app
   .use(bodyParser.json())
   .use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    // res.setHeader('Access-Control-Allow-Origin', '*');
+    // res.setHeader(
+    //   'Access-Control-Allow-Headers',
+    //   'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    // );
+    // res.setHeader('Content-Type', 'application/json');
+    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
   })
   .use('/', require('./routes'));
 
-mongodb.initDb((err, mongodb) => {
+mongodb.initDb((err) => {
   if (err) {
     console.log(err);
   } else {
