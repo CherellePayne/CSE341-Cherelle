b5448c9cc5afdf6dab0e3d05b6c3dbc3c40b7b7b
CherellePayne <cherellelpayne@gmail.com>
2023-05-22 18:30:58 -0600

    swagger routes and proper swagger on package.json

--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,12 @@ const routes = require('express').Router();
 routes.get('/', (req, res) => {
   res.send('Ken Payne');
 });
-// added this line in week 2
+
+routes.use('/', require('./swagger'));
 routes.use('/contacts', require('./contacts.js'));
-module.exports = routes;
+
 
 const testVariable = 'awesome!';
 
-// //Week2
-// const express = require('express');
-// const router = express.Router();
-
-// router.use('/contacts', require('./contacts.js'))
-
 module.exports = routes;
+
